0f1c109d421e7e1a2d61a9c9c607576a77e0d39b
jaeyong <maxwodyd98@gmail.com>
2020-11-21 16:00:09 +0900

    Install RxSwift SwiftJson int cocoapods

---
+++ RxSwiftProject1/Pods/Target Support Files/Pods-RxSwiftProject1/Pods-RxSwiftProject1-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RxSwiftProject1 : NSObject
+@end
+@implementation PodsDummy_Pods_RxSwiftProject1
+@end
---
+++ RxSwiftProject1/RxSwiftProject1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxSwiftProject1
+//
+//  Created by 이재용 on 2020/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
